0f857e9396400821185d62366bd3a770cd445da7
jasoncyu <yujason2@gmail.com>
2016-11-23 12:12:27 -0800

    fix: dir-locals file

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,10 @@
+;; In file
+;; http://stackoverflow.com/questions/7334565/how-can-i-move-php-mode-settings-from-emacs-to-dir-locals-el/7340962#7340962
 ((nil .
-   (
-     (add-to-list 'auto-mode-alist '("\\.js\\'" . react-mode))
-     (eval .
-       '(setq flycheck-javascript-eslint-executable
-          (expand-file-name (concat default-directory "node_modules/.bin/eslint")))))))
+   ((eval .
+       (progn
+         (add-to-list 'auto-mode-alist '("\\.js\\'" . react-mode))
+         (setq flycheck-javascript-eslint-executable
+           (expand-file-name (concat default-directory "node_modules/.bin/eslint")))
+         )
+       ))))
